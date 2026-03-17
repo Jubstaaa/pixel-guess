63c972d4b8dd4676c8aa7528da27241d6196d7e5
İlker Balcılar <ilkerbalcilartr@gmail.com>
2026-03-17 18:11:13 +0300

    fix SPA routing 404 and reorder categories

--- packages/shared/src/data/categories.ts
+++ packages/shared/src/data/categories.ts
@@ -16,34 +16,16 @@ import {
 
 export const categories: Category[] = [
     {
-        slug: 'pokemon',
-        name: 'Pokémon',
-        icon: 'https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg',
-        characterCount: pokemonCharacters.length,
-    },
-    {
-        slug: 'flags',
-        name: 'Flags',
-        icon: 'https://flagcdn.com/w160/un.png',
-        characterCount: flagCharacters.length,
-    },
-    {
-        slug: 'dragon-ball',
-        name: 'Dragon Ball',
-        icon: 'https://dragonball-api.com/characters/goku_normal.webp',
-        characterCount: dragonBallCharacters.length,
-    },
-    {
-        slug: 'naruto',
-        name: 'Naruto',
-        icon: 'https://static.wikia.nocookie.net/naruto/images/d/d6/Naruto_Part_I.png',
-        characterCount: narutoCharacters.length,
-    },
-    {
         slug: 'league-of-legends',
         name: 'League of Legends',
         icon: 'https://vignette.wikia.nocookie.net/leagueoflegends/images/1/12/League_of_Legends_Icon.png',
         characterCount: leagueOfLegendsCharacters.length,
+    },
+    {
+        slug: 'dota-2',
+        name: 'Dota 2',
+        icon: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/global/dota2_logo_symbol.png',
+        characterCount: dota2Characters.length,
     },
     {
         slug: 'valorant',
@@ -52,9 +34,33 @@ export const categories: Category[] = [
         characterCount: valorantCharacters.length,
     },
     {
+        slug: 'flags',
+        name: 'Flags',
+        icon: 'https://flagcdn.com/w160/un.png',
+        characterCount: flagCharacters.length,
+    },
+    {
+        slug: 'pokemon',
+        name: 'Pokémon',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg',
+        characterCount: pokemonCharacters.length,
+    },
+    {
+        slug: 'dragon-ball',
+        name: 'Dragon Ball',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/9/9b/Dragon_Ball_Z_Logo.png',
+        characterCount: dragonBallCharacters.length,
+    },
+    {
+        slug: 'naruto',
+        name: 'Naruto',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/c/c9/Naruto_logo.svg',
+        characterCount: narutoCharacters.length,
+    },
+    {
         slug: 'overwatch',
         name: 'Overwatch',
-        icon: 'https://d15f34w2p8l1cc.cloudfront.net/overwatch/985b06beae46b7ba3ca87d1512d0fc62ca7f206ceca58ef16fc44d43a1cc84ed.png',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Overwatch_2_logo.svg',
         characterCount: overwatchCharacters.length,
     },
     {
@@ -75,27 +81,21 @@ export const categories: Category[] = [
         icon: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Rick_and_Morty.svg',
         characterCount: rickAndMortyCharacters.length,
     },
-    {
-        slug: 'dota-2',
-        name: 'Dota 2',
-        icon: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/global/dota2_logo_symbol.png',
-        characterCount: dota2Characters.length,
-    },
 ]
 
 export const charactersByCategory: Record<
     string,
     typeof leagueOfLegendsCharacters
 > = {
-    pokemon: pokemonCharacters,
+    'league-of-legends': leagueOfLegendsCharacters,
+    'dota-2': dota2Characters,
+    valorant: valorantCharacters,
     flags: flagCharacters,
+    pokemon: pokemonCharacters,
     'dragon-ball': dragonBallCharacters,
     naruto: narutoCharacters,
-    'league-of-legends': leagueOfLegendsCharacters,
-    valorant: valorantCharacters,
     overwatch: overwatchCharacters,
     fortnite: fortniteCharacters,
     'genshin-impact': genshinCharacters,
     'rick-and-morty': rickAndMortyCharacters,
-    'dota-2': dota2Characters,
 }
